cf39b43c0572657cdb9afbe4d2d57a32ea0327a0
William Santana <euwilliamsantana@gmail.com>
2024-01-26 22:28:16 -0300

    feat: Structure React-Router-dom: OKAY

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { ThemeProvider } from 'styled-components'
 
 import { defaultTheme } from './styles/theme/default'
+import { BrowserRouter } from 'react-router-dom'
 import { GlobalStyles } from './styles/globalStyles'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyles />
     </ThemeProvider>
   )
