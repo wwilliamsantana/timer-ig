62c5a8721b14d0ae9762e23b873b89ca3060810e
William Santana <euwilliamsantana@gmail.com>
2024-01-28 16:31:57 -0300

    feat: Countdown working

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import * as zod from 'zod'
+import { differenceInSeconds } from 'date-fns'
 import { zodResolver } from '@hookform/resolvers/zod'
 import {
   Button,
@@ -11,6 +12,7 @@ import {
   Separator,
   TaskInput,
 } from './styles'
+import { useEffect, useState } from 'react'
 
 const schemaValidation = zod.object({
   task: zod.string().min(1, 'Informe uma tarefa'),
@@ -22,7 +24,18 @@ const schemaValidation = zod.object({
 
 type FormDataProps = zod.infer<typeof schemaValidation>
 
+interface CyclesProps {
+  id: string
+  minuteAmout: number
+  task: string
+  startDate: Date
+}
+
 export function Home() {
+  const [cycles, setCyles] = useState<CyclesProps[]>([])
+  const [isActiveCycle, setActiveCycle] = useState<string | null>(null)
+  const [isSecondsPassed, setIsSecondsPassed] = useState(0)
+
   const { register, handleSubmit, watch, reset } = useForm<FormDataProps>({
     resolver: zodResolver(schemaValidation),
     defaultValues: {
@@ -31,11 +44,41 @@ export function Home() {
     },
   })
   const disabledButtonControl = watch('task')
+  const cycleActivated = cycles.find((cycle) => cycle.id === isActiveCycle)
+
+  const totalSeconds = cycleActivated ? cycleActivated.minuteAmout * 60 : 0
+  const currentSeconds = cycleActivated ? totalSeconds - isSecondsPassed : 0
+
+  const minutosAmout = Math.floor(totalSeconds / 60)
+  const secondsAmout = currentSeconds % 60
+
+  const minutes = String(minutosAmout).padStart(2, '0')
+  const seconds = String(secondsAmout).padStart(2, '0')
 
   function handleSubmitCountdown(data: FormDataProps) {
-    console.log(data)
+    const id = String(new Date().getTime())
+    const cyclesCurrent: CyclesProps = {
+      id,
+      minuteAmout: data.minutesAmout,
+      task: data.task,
+      startDate: new Date(),
+    }
+
+    setCyles((state) => [...state, cyclesCurrent])
+    setActiveCycle(id)
+
     reset()
   }
+
+  useEffect(() => {
+    if (cycleActivated) {
+      setInterval(() => {
+        setIsSecondsPassed(
+          differenceInSeconds(new Date(), cycleActivated.startDate),
+        )
+      }, 1000)
+    }
+  }, [cycleActivated])
 
   return (
     <HomeContainer>
@@ -57,16 +100,16 @@ export function Home() {
             max={60}
             min={5}
             placeholder="00"
-            {...register('minutesAmout')}
+            {...register('minutesAmout', { valueAsNumber: true })}
           />
           <span>minutos.</span>
         </FormContainer>
         <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountdownContainer>
 
         <Button disabled={!disabledButtonControl} type="submit">
